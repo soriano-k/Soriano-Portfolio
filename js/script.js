fcd06d866655fceecf7fb47e37e78ed579c861a4
soriano-k <kylsoriano@gmail.com>
2024-06-14 22:10:49 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========== typing animation ========== */
+var typed = new typed(".typing",{
+    strings: ["","3rd year Computer Science Student", "Graphics Designer", "GFX Editor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
